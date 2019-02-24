fe0013a01885f7e4640fc2e5997c4e54949d8ed5
kosyanenkoks <kosyanenko.ks@gmail.com>
2019-02-24 19:58:19 +0200

    task 11 is done

---
+++ task-11.js
@@ -0,0 +1,42 @@
+var selector = document.body.appendChild (
+    document.createElement ( 'input' )
+);
+
+selector.type = 'file';
+selector.multiple = true;
+selector.id = 'selectImages';
+selector.style.display = 'none';
+
+var label = document.body.appendChild (
+    document.createElement ( 'label' )
+);
+label.htmlFor = 'selectImages';
+label.innerText = 'Select images';
+
+var promise = function ( imageFile ) {
+    return new Promise(
+        (resolve, reject) => {
+            if (imageFile.type.match('image.*')) {
+                var reader = new FileReader();
+                reader.onload = function(event) {
+                    resolve(this.result);
+                };
+                reader.readAsDataURL(selector.files[0]);
+            } else {
+                reject('Выбранный файл не является изображением')
+            }
+        }
+    )
+};
+
+selector.onchange = function ( event ) {
+    for ( var file of event.target.files ) {
+        promise ( file )
+            .then ( result => {
+                var picture = document.createElement ( "img" );
+                document.body.appendChild( picture );
+                picture.src = result
+            })
+            .catch ( error => console.error ( error ) )
+    }
+};
